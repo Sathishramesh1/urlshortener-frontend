df9211dfbe9247a8f01d0596965dc6252b43b280
Sathishramesh1 <sathishmech2k13@gmail.com>
2023-11-22 16:16:06 +0530

    layout-update

--- urlshortner-app/src/App.jsx
+++ urlshortner-app/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import Layout from './Components/Layout'
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>hello</div>
-    </>
+ 
+      <div className='container base'> 
+       <Layout></Layout>
+       </div> 
+    
   )
 }
 
---
+++ urlshortner-app/src/Components/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../App.css'
+
+import NavScrollExample from './NavBar'
+
+function Layout() {
+  return (
+  
+
+<>
+
+<NavScrollExample/>
+<div className='home'>
+    <div className='left-content'>
+
+        
+    </div>
+    <div className='right-content'>name</div>
+</div>
+</>
+   
+ 
+  )
+}
+
+export default Layout
